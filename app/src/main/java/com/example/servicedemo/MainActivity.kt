94a1f1533c2c844bf1c00d28a1899d9373e1dc70
danil <daniltirskk@gmail.com>
2022-05-29 17:29:41 +0800

    Initial

--- app/src/main/java/com/example/servicedemo/MainActivity.kt
+++ app/src/main/java/com/example/servicedemo/MainActivity.kt
@@ -4,41 +4,108 @@ import android.app.Notification.VISIBILITY_PUBLIC
 import android.app.NotificationManager
 import android.app.NotificationChannel
 import android.content.Context
+import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.*
+import android.widget.ArrayAdapter
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 
 class MainActivity : AppCompatActivity() {
+    var currencyToTrack: Int = 0
+    var is_running = false
+    lateinit var list: ListView
+//    lateinit var listItems: MutableList<String>
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val CHANNEL_ID = "CHANNEL_ID"
-        val channel = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            NotificationChannel(CHANNEL_ID,"name",NotificationManager.IMPORTANCE_DEFAULT)
+        val spinner: Spinner = findViewById(R.id.spinner)
+        var spinnerItems = mutableListOf<String>("BTC","ETH","USDT","BNB","DOGE")
+        val spinnerAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, spinnerItems)
+        spinner.adapter = spinnerAdapter
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parentView: AdapterView<*>?,
+                selectedItemView: View?,
+                position: Int,
+                id: Long
+            ) {
+                currencyToTrack = position
+                crypto = spinnerItems[currencyToTrack]
+            }
+
+            override fun onNothingSelected(parentView: AdapterView<*>?) {
+                // your code here
+            }
+        }
+
+        var button = findViewById<Button>(R.id.btnSubscribeToRate)
+
+        button.setOnClickListener(){
+            onClick(it)
+        }
+
+        list = findViewById(R.id.list)
+//        listItems = mutableListOf<String>("EMPTY")
+        listAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, history)
+        list.adapter = listAdapter
 
 
+
+
+
+//        val CHANNEL_ID = "CHANNEL_ID"
+//        val channel = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+//            NotificationChannel(CHANNEL_ID,"name",NotificationManager.IMPORTANCE_DEFAULT)
+//
+//
+//        } else {
+//            TODO("VERSION.SDK_INT < O")
+//        }
+//
+//        channel. description = "channelDescription"
+//        (getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager)
+//            .createNotificationChannel(channel)
+//
+//
+//        val builder = NotificationCompat.Builder(this, CHANNEL_ID)
+//            .setSmallIcon(R.drawable.cat)
+//            .setContentTitle("My title")
+//            .setContentText("My content for this notification")
+//            .setPriority(NotificationCompat.PRIORITY_HIGH)
+//
+//
+//        if (Build.VERSION.SDK_INT >= 21) builder.setVibrate(LongArray(0))
+//        with(NotificationManagerCompat.from(this)) {
+//            notify(7, builder.build())
+//        }
+    }
+
+    fun onClick(view: View){
+        if (!is_running){
+            intent = Intent(this, RateCheckService::class.java)
+            startService(intent)
+            is_running = true
         } else {
-            TODO("VERSION.SDK_INT < O")
+            intent = Intent(this, RateCheckService::class.java)
+            startService(intent)
+            history.clear()
+            history.add("EMPTY")
+            listAdapter.notifyDataSetChanged();
         }
-        
-        channel. description = "channelDescription"
-        (getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager)
-            .createNotificationChannel(channel)
+    }
 
-
-        val builder = NotificationCompat.Builder(this, CHANNEL_ID)
-            .setSmallIcon(R.drawable.cat)
-            .setContentTitle("My title")
-            .setContentText("My content for this notification")
-            .setPriority(NotificationCompat.PRIORITY_HIGH)
-
-
-        if (Build.VERSION.SDK_INT >= 21) builder.setVibrate(LongArray(0))
-        with(NotificationManagerCompat.from(this)) {
-            notify(7, builder.build())
-        }
+    companion object{
+        var history = mutableListOf<String>("EMPTY")
+        var crypto = "BTC"
+        var lastPrice = 0.0
+        lateinit var listAdapter: ArrayAdapter<String>
     }
 }
--- app/src/main/java/com/example/servicedemo/RateCheckService.kt
+++ app/src/main/java/com/example/servicedemo/RateCheckService.kt
@@ -1,19 +1,81 @@
 package com.example.servicedemo
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.app.Service
+import android.content.Context
 import android.content.Intent
+import android.os.Build
+import android.os.Handler
 import android.os.IBinder
+import android.util.Log
 import androidx.annotation.Nullable
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import org.json.JSONObject
+import java.net.URL
+import java.net.UnknownHostException
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
+import java.util.*
 
 class RateCheckService : Service() {
 
-    @Nullable
-    override fun onBind(intent: Intent)  = null
-    override fun onCreate() {
-        super.onCreate()
+    private lateinit var mHandler: Handler
+    private lateinit var mRunnable: Runnable
+
+    override fun onBind(intent: Intent): IBinder {
+        TODO("Return the communication channel to the service.")
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        return super.onStartCommand(intent, flags, startId)
+        mHandler = Handler()
+        mRunnable = Runnable {
+            GlobalScope.launch(Dispatchers.IO) {
+                loadCrypto()
+            }
+        }
+        mHandler.postDelayed(mRunnable, 5000)
+        return Service.START_STICKY
+    }
+
+    suspend fun loadCrypto() {
+        val API_KEY = "b54bcf4d-1bca-4e8e-9a24-22ff2c3d462c"
+        val CRYPTO = MainActivity.crypto
+        val URL = "https://min-api.cryptocompare.com/data/price?fsym=$CRYPTO&tsyms=USD&api_key=$API_KEY"
+        try {
+            val data = URL(URL).readText()//.getContent() as InputStream
+            Log.d("stream", data)
+            val jObject = JSONObject(data)
+            val price = jObject.getDouble("USD");
+            var time = SimpleDateFormat("dd/M/yyyy hh:mm:ss").format(Date())
+
+            var newEntry = "Price: $price$   Date: $time"
+
+
+            if (MainActivity.history[0] == "EMPTY"){
+                MainActivity.history.clear()
+            }
+            if (MainActivity.lastPrice != price){
+                MainActivity.lastPrice = price
+                if (MainActivity.history.size < 15){
+                    MainActivity.history.add(0,newEntry)
+                } else {
+                    MainActivity.history.removeAt(14)
+                    MainActivity.history.add(0,newEntry)
+                }
+            }
+
+            GlobalScope.launch(Dispatchers.Main) {
+                MainActivity.listAdapter.notifyDataSetChanged();
+            }
+        } catch (e : UnknownHostException){
+            Log.d("mytag", "No internet")
+        } catch (e : Exception) {
+            Log.d("mytag", "Unknown exception: $e")
+        }
+        mHandler.postDelayed(mRunnable, 5000)
     }
 }
